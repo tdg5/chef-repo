65b3f7edb7fa14ac56f94374e21058d4b47520eb
Danny Guinther <dannyguinther@gmail.com>
2024-05-02 21:56:28 -0400

    Add vim cookbook and add to universal policy

--- policyfiles/universal.rb
+++ policyfiles/universal.rb
@@ -5,9 +5,11 @@ default_source :supermarket
 cookbook 'apt', '~> 7.5.17', :supermarket
 cookbook 'bash_config', path: '../cookbooks/bash_config'
 cookbook 'htop', path: '../cookbooks/htop'
+cookbook 'vim', '~> 2.1.20', :supermarket
 
 run_list(
   'apt',
   'htop',
   'bash_config',
+  'vim',
 )
