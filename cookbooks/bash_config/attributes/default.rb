55316a06bc44dc709d7c7bd95b1c1275e471b031
Danny Guinther <dannyguinther@gmail.com>
2024-05-19 13:52:56 -0400

    Fix type in platform name

--- cookbooks/bash_config/attributes/default.rb
+++ cookbooks/bash_config/attributes/default.rb
@@ -4,4 +4,4 @@ default['bash']['completion'] = [
 
 default['bash']['bashrc']['extra_sources'] = {}
 default['bash']['completion_dir']['mac_os_x'] = '/opt/homebrew/etc/bash_completion.d'
-default['bash']['completion_dir']['ubtuntu'] = '/etc/bash_completion.d'
+default['bash']['completion_dir']['ubuntu'] = '/etc/bash_completion.d'
