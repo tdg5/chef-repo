ea0a388f3fda5f4a8730c64eb106a7aeea9321e5
Danny Guinther <dannyguinther@gmail.com>
2024-05-19 09:55:31 -0400

    Add a policy for inner-beast

---
+++ policyfiles/inner-beast.rb
@@ -0,0 +1,24 @@
+name 'inner-beast'
+
+default_source :supermarket
+
+include_policy 'ubuntu-base', path: '.'
+
+cookbook 'noop', path: '../cookbooks/noop'
+
+run_list ['noop']
+
+username = 'tdg5'
+group = username
+
+default["user"] = {
+  email: 'dannyguinther@gmail.com',
+  group: group,
+  home_directory: '/home/tdg5',
+  username: username,
+}
+
+default["root_user"] = {
+  group: "root",
+  username: "root",
+}
