c0471609419e7e217d37bcbd3f4e70a82cb51cd8
Danny Guinther <dannyguinther@gmail.com>
2024-05-21 11:35:08 -0400

    Add .zprofile to config::zsh

--- cookbooks/config/recipes/zsh.rb
+++ cookbooks/config/recipes/zsh.rb
@@ -2,6 +2,12 @@ home_directory = node['user']['home_directory']
 username = node['user']['username']
 group_name = node['user']['group']
 
+cookbook_file "#{home_directory}/.zprofile" do
+  group group_name
+  mode 0644
+  owner username
+end
+
 cookbook_file "#{home_directory}/.zshrc" do
   group group_name
   mode 0644
