b8c00cf9b719a25828bb9993c56e849c85041bd0
Danny Guinther <dannyguinther@gmail.com>
2024-05-21 13:12:05 -0400

    Fix spotify apt repository component name

--- cookbooks/spotify/recipes/default.rb
+++ cookbooks/spotify/recipes/default.rb
@@ -1,6 +1,6 @@
 if node['platform_family'] == 'debian'
   apt_repository 'spotify' do
-    components ['free']
+    components ['non-free']
     distribution 'stable'
     key 'https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg'
     uri 'http://repository.spotify.com'
