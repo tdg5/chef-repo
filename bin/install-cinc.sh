499418658f3d830370262964d6129a517a25ef01
Danny Guinther <dannyguinther@gmail.com>
2024-05-22 10:28:38 -0400

    Add workaround for installing cinc-workstation on MacOS+arm64

--- bin/install-cinc.sh
+++ bin/install-cinc.sh
@@ -10,7 +10,12 @@ fi
 # Install cinc workstation
 # From: https://cinc.sh/start/workstation/
 if ! `which knife`; then
-  curl -L https://omnitruck.cinc.sh/install.sh | bash -s -- -P cinc-workstation -v 24
+  if [ `uname -s` = 'Darwin' -a `uname -m` = 'arm64' ]; then
+    # For some reason the install can't find the right download on MacOS + ARM64
+    curl -L https://omnitruck.cinc.sh/install.sh | sudo bash -s -- -P cinc-workstation -v 24 -l 'https://downloads.cinc.sh/files/stable/cinc-workstation/24.4.1064/mac_os_x/11/cinc-workstation-24.4.1064-1.arm64.dmg'
+  else
+    curl -L https://omnitruck.cinc.sh/install.sh | bash -s -- -P cinc-workstation -v 24
+  fi
 fi
 
 # Install cinc client after cinc workstation since it often has newer versions
