21b6428dad7ea2a2aa3f57cae3c775047941cc03
Danny Guinther <danny@phave.com>
2024-05-22 13:13:03 -0400

    Update install-cinc script to not attempt workstation install on linux+arm64

--- bin/install-cinc.sh
+++ bin/install-cinc.sh
@@ -10,9 +10,15 @@ fi
 # Install cinc workstation
 # From: https://cinc.sh/start/workstation/
 if ! `which knife`; then
-  if [ `uname -s` = 'Darwin' -a `uname -m` = 'arm64' ]; then
-    # For some reason the install can't find the right download on MacOS + ARM64
-    curl -L https://omnitruck.cinc.sh/install.sh | sudo bash -s -- -P cinc-workstation -v 24 -l 'https://downloads.cinc.sh/files/stable/cinc-workstation/24.4.1064/mac_os_x/11/cinc-workstation-24.4.1064-1.arm64.dmg'
+  ARCHITECTURE=$(uname -m)
+  if [ $ARCHITECTURE = 'arm64' -o $ARCHITECTURE = 'aarch64' ]; then
+    OPERATING_SYSTEM=$(uname -s)
+    if [ "$OPERATING_SYSTEM" = 'Darwin' ]; then
+      # For some reason the install can't find the right download on MacOS + ARM64
+      curl -L https://omnitruck.cinc.sh/install.sh | sudo bash -s -- -P cinc-workstation -v 24 -l 'https://downloads.cinc.sh/files/stable/cinc-workstation/24.4.1064/mac_os_x/11/cinc-workstation-24.4.1064-1.arm64.dmg'
+    elif [ "$OPERATING_SYSTEM" = 'Linux' ]; then
+      echo "cinc-workstation isn't supported on Linux ARM64; skipping..."
+    fi
   else
     curl -L https://omnitruck.cinc.sh/install.sh | bash -s -- -P cinc-workstation -v 24
   fi
