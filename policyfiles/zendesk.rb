16b9c6cc1f76544a2caabf05e187d6ecd8c780de
Danny Guinther <dannyguinther@gmail.com>
2026-06-06 16:31:28 +0000

    Add standard shell environment to zendesk policy

--- policyfiles/zendesk.rb
+++ policyfiles/zendesk.rb
@@ -2,10 +2,25 @@ name 'zendesk'
 
 default_source :supermarket
 
-cookbook 'noop', path: '../cookbooks/noop'
+cookbook 'bash_config', path: '../cookbooks/bash_config'
+cookbook 'config', path: '../cookbooks/config'
+cookbook 'exuberant-ctags', path: '../cookbooks/exuberant-ctags'
+cookbook 'liquidprompt', path: '../cookbooks/liquidprompt'
+cookbook 'simple-packages', path: '../cookbooks/simple-packages'
+cookbook 'vim_plugins', path: '../cookbooks/vim_plugins'
+cookbook 'vim_wrapper', path: '../cookbooks/vim_wrapper'
 
 run_list(
-  'noop',
+  # Standard interactive shell environment (subset of ubuntu-base; no
+  # docker/desktop, server-safe on 26.04).
+  'bash_config',
+  'config::git',
+  'config::sshagent',
+  'vim_wrapper',
+  'vim_plugins',
+  'config::vim',
+  'config::tmux',
+  'liquidprompt',
 )
 
 username = 'tdg5'
@@ -22,3 +37,10 @@ default['root_user'] = {
   group: 'root',
   username: 'root',
 }
+
+# Files sourced from the generated ~/.bashrc. The template guards each with
+# `[ -e <path> ]`, so entries whose file is absent are simply skipped.
+default['bash']['bashrc']['extra_sources']['standard bash aliases'] = '~/.bash_aliases'
+default['bash']['bashrc']['extra_sources']['extra, env dependent aliases'] = '~/.bash/extra_aliases'
+default['bash']['bashrc']['extra_sources']['liquidprompt'] = '/opt/liquidprompt/liquidprompt'
+default['bash']['bashrc']['extra_sources']['ssh agent'] = '~/.sshagentrc'
