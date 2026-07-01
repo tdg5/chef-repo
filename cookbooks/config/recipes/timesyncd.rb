55e81ba34b9fb2e83f5d8547ab6d2306c12a2e19
Danny Guinther <dannyguinther@gmail.com>
2026-07-01 14:07:22 -0400

    Don't install timesync recipe on ubuntu 26.04 hosts

--- cookbooks/config/recipes/timesyncd.rb
+++ cookbooks/config/recipes/timesyncd.rb
@@ -1,13 +1,27 @@
-root_group = node['root_user']['group']
-root_username = node['root_user']['username']
+# systemd-timesyncd isn't present on every platform. Newer Ubuntu images
+# (e.g. 26.04) ship chrony as the default time daemon, and chrony displaces
+# systemd-timesyncd -- so its unit doesn't exist and restarting it fails with
+# "Unit not found". Only manage timesyncd when its unit is actually installed;
+# where it isn't, another daemon (chrony) already handles time sync. This
+# self-heals if a future image ships timesyncd again.
+timesyncd_unit = 'systemd-timesyncd.service'
 
-cookbook_file "/etc/systemd/timesyncd.conf" do
-  group root_group
-  mode '0644'
-  notifies :restart, 'systemd_unit[systemd-timesyncd.service]', :immediately
-  owner root_username
-end
+if ::File.exist?("/usr/lib/systemd/system/#{timesyncd_unit}")
+  root_group = node['root_user']['group']
+  root_username = node['root_user']['username']
 
-systemd_unit 'systemd-timesyncd.service' do
-  action :nothing
+  cookbook_file '/etc/systemd/timesyncd.conf' do
+    group root_group
+    mode '0644'
+    notifies :restart, "systemd_unit[#{timesyncd_unit}]", :immediately
+    owner root_username
+  end
+
+  systemd_unit timesyncd_unit do
+    action :nothing
+  end
+else
+  log "skipping #{timesyncd_unit} config: unit not installed (another time daemon, e.g. chrony, handles sync here)" do
+    level :info
+  end
 end
